0ad559e90d083025c72efc0dcc60fb6a923ac270
Malte Franken <exxamalte@users.noreply.github.com>
2024-07-04 23:11:10 +1000

    add log configuration

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 """Teams bridge application."""
 import logging
+import os
 
+import platformdirs
+
+from teams_bridge.consts import APPLICATION_NAME
 from teams_bridge.teams_bridge import TeamsBridge
 from teams_bridge.__version__ import __version__
 
 
 def main():
     """Execute command line tool."""
-    # Set up logger
-    logging.basicConfig(level=logging.DEBUG)
+    # Set up logger.
+    logfile = os.path.join(platformdirs.user_log_dir(appname=APPLICATION_NAME, ensure_exists=True), "app.log")
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(levelname)s:%(name)s:%(module)s:%(message)s',
+                        filename=logfile)
     logger = logging.getLogger(__name__)
     logger.info(f"Teams Bridge (version {__version__})...")
+    # Start application.
     app = TeamsBridge()
     app.run()
 
