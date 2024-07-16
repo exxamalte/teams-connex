7a2b47f97a151e3c5041ac30e33b81249ef01fa9
Malte Franken <exxamalte@users.noreply.github.com>
2024-07-16 19:42:27 +1000

    configuration write file tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,11 @@ from unittest import mock
 from unittest.mock import mock_open
 
 from teams_connex.app import TeamsConnex
+from tests.utils import concatenate_writes
 
 
 @mock.patch("os.path.isfile")
-def test_app_configuration_defaults(mock_isfile):
+def test_app_configuration_read_defaults(mock_isfile):
     """Test app configuration file defaults."""
     mock_isfile.return_value = True
     m = mock_open(read_data="")
@@ -19,8 +20,8 @@ def test_app_configuration_defaults(mock_isfile):
 
 
 @mock.patch("os.path.isfile")
-def test_app_configuration_values(mock_isfile):
-    """Test app configuration file defaults."""
+def test_app_configuration_read_custom_values(mock_isfile):
+    """Test app configuration file with custom values."""
     mock_isfile.return_value = True
     m = mock_open(
         read_data="settings:\n"
@@ -33,3 +34,37 @@ def test_app_configuration_values(mock_isfile):
         assert app.token == "test-token-1"
         assert app.webhook_uri == "https://my.webhook.url/"
         assert app.debug_mode
+
+
+def test_app_configuration_write_values():
+    """Test write to app configuration file."""
+    m = mock_open(read_data="")
+    with mock.patch("builtins.open", m):
+        app = TeamsConnex()
+        # Token
+        app.token = "test-token-2"
+        m.assert_called_with(app.configuration_file, mode="w")
+        writes = m.return_value.write.mock_calls
+        result = concatenate_writes(writes)
+        assert result == ("settings:\n" "  teams_token: test-token-2\n")
+        # Webhook URL
+        m.reset_mock()
+        app.webhook_uri = "https://your.webhook.url/"
+        writes = m.return_value.write.mock_calls
+        result = concatenate_writes(writes)
+        assert result == (
+            "settings:\n"
+            "  teams_token: test-token-2\n"
+            "  webhook_uri: https://your.webhook.url/\n"
+        )
+        # Debug mode
+        m.reset_mock()
+        app.debug_mode = True
+        writes = m.return_value.write.mock_calls
+        result = concatenate_writes(writes)
+        assert result == (
+            "settings:\n"
+            "  debug_mode: true\n"
+            "  teams_token: test-token-2\n"
+            "  webhook_uri: https://your.webhook.url/\n"
+        )
