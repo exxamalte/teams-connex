0aa3198d2f6a822278201b1650e6339108270765
Malte Franken <exxamalte@users.noreply.github.com>
2024-07-16 19:22:43 +1000

    configuration file tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,12 +7,29 @@ from teams_connex.app import TeamsConnex
 
 
 @mock.patch("os.path.isfile")
-def test_app(mock_isfile):
-    """Test app."""
-
+def test_app_configuration_defaults(mock_isfile):
+    """Test app configuration file defaults."""
     mock_isfile.return_value = True
-
-    m = mock_open(read_data="token: abcdefg")
+    m = mock_open(read_data="")
     with mock.patch("builtins.open", m):
         app = TeamsConnex()
-        assert app.token == "abcdefg"
+        assert app.token == ""
+        assert app.webhook_uri == ""
+        assert not app.debug_mode
+
+
+@mock.patch("os.path.isfile")
+def test_app_configuration_values(mock_isfile):
+    """Test app configuration file defaults."""
+    mock_isfile.return_value = True
+    m = mock_open(
+        read_data="settings:\n"
+        "  teams_token: test-token-1\n"
+        "  webhook_uri: https://my.webhook.url/\n"
+        "  debug_mode: true\n"
+    )
+    with mock.patch("builtins.open", m):
+        app = TeamsConnex()
+        assert app.token == "test-token-1"
+        assert app.webhook_uri == "https://my.webhook.url/"
+        assert app.debug_mode
